83eafd824d91eea059d47c4aa20f0879fd73853a
Veda Kailasam <45158591+22veda@users.noreply.github.com>
2020-02-16 12:31:30 +0530

    Add files via upload

---
+++ templates/footer.php
@@ -0,0 +1,76 @@
+<footer>
+  <div class="container">
+    <div class="row">
+      <div class="col-sm-3 footer-block">
+        <h5 class="footer-title">Information</h5>
+        <ul class="list-unstyled ul-wrapper">
+          <li><a href="about-us.html">About Us</a></li>
+          <li><a href="checkout.html">Delivery Information</a></li>
+          <li><a href="#">Privacy Policy</a></li>
+          <li><a href="#">Terms &amp; Conditions</a></li>
+        </ul>
+      </div>
+      <div class="col-sm-3 footer-block">
+        <h5 class="footer-title">Customer Service</h5>
+        <ul class="list-unstyled ul-wrapper">
+          <li><a href="contact.html">Contact Us</a></li>
+          <li><a href="#">Returns</a></li>
+          <li><a href="#">Site Map</a></li>
+          <li><a href="#">Wish List</a></li>
+        </ul>
+      </div>
+      <div class="col-sm-3 footer-block">
+        <h5 class="footer-title">Extras</h5>
+        <ul class="list-unstyled ul-wrapper">
+          <li><a href="#">Brands</a></li>
+          <li><a href="gift.html">Gift Vouchers</a></li>
+          <li><a href="affiliate.html">Affiliates</a></li>
+          <li><a href="#">Specials</a></li>
+        </ul>
+      </div>
+      <div class="col-sm-3 footer-block">
+        <div class="content_footercms_right">
+          <div class="footer-contact">
+            <h5 class="contact-title footer-title">Contact Us</h5>
+            <ul class="ul-wrapper">
+              <li><i class="fa fa-map-marker"></i><span class="location2"> Warehouse & Offices,<br>
+                12345 Street name, California<br>
+                USA</span></li>
+              <li><i class="fa fa-envelope"></i><span class="mail2"><a href="#">info@localhost.com</a></span></li>
+              <li><i class="fa fa-mobile"></i><span class="phone2">+91 0987-654-321</span></li>
+            </ul>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+  <a id="scrollup">Scroll</a> </footer>
+<div class="footer-bottom">
+  <div class="container">
+    <div class="copyright">Powered By &nbsp;<a class="yourstore" href="http://www.lionode.com/">Agro Field &copy; 2017 </a> </div>
+    <div class="footer-bottom-cms">
+      <div class="footer-payment">
+        <ul>
+          <li class="mastero"><a href="#"><img alt="" src="/HomeAssets/image/payment/mastero.jpg"></a></li>
+          <li class="visa"><a href="#"><img alt="" src="/HomeAssets/image/payment/visa.jpg"></a></li>
+          <li class="currus"><a href="#"><img alt="" src="/HomeAssets/image/payment/currus.jpg"></a></li>
+          <li class="discover"><a href="#"><img alt="" src="/HomeAssets/image/payment/discover.jpg"></a></li>
+          <li class="bank"><a href="#"><img alt="" src="/HomeAssets/image/payment/bank.jpg"></a></li>
+        </ul>
+      </div>
+    </div>
+  </div>
+</div>
+<script src="/HomeAssets/javascript/jquery-2.1.1.min.js" type="text/javascript"></script>
+<script src="/HomeAssets/bootstrap/js/bootstrap.min.js" type="text/javascript"></script>
+<script src="/HomeAssets/javascript/jstree.min.js" type="text/javascript"></script>
+<script src="/HomeAssets/javascript/template.js" type="text/javascript" ></script>
+<script src="/HomeAssets/javascript/common.js" type="text/javascript"></script>
+<script src="/HomeAssets/javascript/global.js" type="text/javascript"></script>
+<script src="/HomeAssets/owl-carousel/owl.carousel.min.js" type="text/javascript"></script>
+<script src="/HomeAssets/javascript/parally.js"></script> 
+<script>
+$('.parallax').parally({offset: -40});
+</script>
+</body>
+</html>
---
+++ templates/header1.php
@@ -0,0 +1,95 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+<title>Agro Field</title>
+<meta http-equiv="content-type" content="text/html;charset=utf-8" />
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<meta name="description" content="e-commerce site well design with responsive view." />
+<meta http-equiv="X-UA-Compatible" content="IE=edge">
+<link href="https://fonts.googleapis.com/css?family=Raleway|Roboto:300,400,500,700&display=swap" rel="stylesheet">
+<link href="/HomeAssets/bootstrap/css/bootstrap.min.css" rel="stylesheet" media="screen" />
+<link href="/HomeAssets/font-awesome/css/font-awesome.min.css" rel="stylesheet" type="text/css" />
+<link href="/HomeAssets/css/stylesheet.css?v=1.1" rel="stylesheet">
+<link href="/HomeAssets/css/responsive.css" rel="stylesheet">
+<link href="/HomeAssets/owl-carousel/owl.carousel.css" type="text/css" rel="stylesheet" media="screen" />
+<link href="/HomeAssets/owl-carousel/owl.transitions.css" type="text/css" rel="stylesheet" media="screen" />
+</head>
+<body>
+<div class="preloader loader" style="display: block; background:#f2f2f2;"> <img src="/HomeAssets/image/loader.gif"  alt="#"/></div>
+<header>
+  <div class="header-top">
+    <div class="container">
+      <div class="row">
+        <div class="col-sm-12">
+          <div class="top-left pull-left">
+            </div>
+          <div class="top-right pull-right">
+            <div id="top-links" class="nav pull-right">
+              <ul class="list-inline">
+			  <?php session_start(); ?>
+			  <?php if(isset($_SESSION["logged_in"])==false) { session_destroy();?>
+			  <li><a href="reg.php"><i class="fa fa-user"></i> Register</a></li>
+              <li><a href="login.php"><i class="fa fa-lock"></i> Login</a></li>
+			  <?php } else { 
+			  
+			  ?>
+			  <li class="dropdown"><a href="#" title="My Account" class="dropdown-toggle" data-toggle="dropdown"> <i class="fa fa-user"></i><span><?php echo $_SESSION["userid"] ?></span> <span class="caret"></span></a>
+                  <ul class="dropdown-menu dropdown-menu-right">
+                    <li><a href="base.php">My products</a></li>
+					<li><a href="alloffersfarmer.php">All products</a></li>
+					<li><a href="createoffer.php">Upload new product</a></li>
+					<li><a href="logout.php">Logout</a></li>
+                  </ul>
+                </li>
+
+			  <?php } ?>
+                
+              </ul>
+              
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+  <div class="container">
+    <div class="header-inner">
+      <div class="col-sm-4 col-xs-6 header-left">
+        <div class="shipping">
+          <div class="shipping-img"></div>
+          <div class="shipping-text">+91 9121012208<span class="shipping-detail">Week From 9:00am To 7:00pm</span></div>
+        </div>
+      </div>
+      <div class="col-sm-4 col-xs-12 header-middle">
+        <div class="header-middle-top">
+          <div id="logo"> <a href="/">
+		  <div style="padding:10px;border-radius:10px;background:#2C6700">
+		  <h1 style="font-size:4rem;color:#fff">Agro Field</h1></a> </div>
+        </div>
+      </div>
+	  </div>
+      <div class="col-sm-4 col-xs-12 header-right">
+      </div>
+    </div>
+  </div>
+</header>
+<nav id="menu" class="navbar">
+  <div class="nav-inner container">
+    <div class="navbar-header"><span id="category" class="visible-xs">Categories</span>
+      <button type="button" class="btn btn-navbar navbar-toggle" ><i class="fa fa-bars"></i></button>
+    </div>
+    <div class="navbar-collapse">
+      <ul class="main-navigation">
+        <li><a href="farmerlogin.php"   class="parent"  >Home</a> </li>
+        <li><a href="base.php"   class="parent" >My products</a>
+		<ul>
+            <li><a href="current.php">Current crops</a></li>
+            <li><a href="history.php">History</a></li>
+          </ul></li>
+		<li><a href="alloffersfarmer.php">All products</a></li>
+		<li><a href="createnewoffer.php">Upload new product</a></li>
+        <li><a href="contact.html" >Contact Us</a> </li>
+      </ul>
+    </div>
+  </div>
+</nav>
